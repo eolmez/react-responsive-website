9af1d91058ed4c64832932235d740f52f73770d9
Emre Ölmez <eolmez025@gmail.com>
2022-05-28 15:44:50 +0300

    new start

---
+++ src/pages/signin.jsx
@@ -0,0 +1,13 @@
+import ScrollToTop from "../components/ScrollToTop";
+import SignIn from "../components/Signin";
+
+const SigninPage = () => {
+  return (
+    <div>
+      <ScrollToTop />
+      <SignIn />
+    </div>
+  );
+};
+
+export default SigninPage;
